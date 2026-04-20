3b27477308aefee4fd0412c572e1e1521df64fca
samarthjain0207 <samarthjain0207@gmail.com>
2026-04-20 13:13:27 +0530

    EmptyBoard

--- TicTacToe_Agipp.java
+++ TicTacToe_Agipp.java
@@ -1,27 +1,21 @@
-public class TicTacToe_App {
-
+public class TicTacToe_Agipp {
     static char[][] board = new char[3][3];
-
     public static void main(String[] args) {
         initializeBoard();
         printBoard();
     }
-
-    // Initialize board with '-'
     static void initializeBoard() {
         for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
+            for (int col=0; col<3;col++) {
                 board[row][col] = '-';
             }
         }
     }
-
-    // Print board in grid format
     static void printBoard() {
         System.out.println("-------------");
         for (int row = 0; row < 3; row++) {
             System.out.print("| ");
-            for (int col = 0; col < 3; col++) {
+            for (int col=0;col<3;col++) {
                 System.out.print(board[row][col] + " | ");
             }
             System.out.println();
